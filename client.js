7d24625532a1b0b2aa97b770d3992ab9123a56b0
Andrew Nguyen <anguyen1320@gmail.com>
2020-03-11 16:46:42 -0500

    completed assignment

--- client.js
+++ client.js
@@ -1,32 +1,32 @@
 const employees = [
   {
-    name: 'Atticus',
-    employeeNumber: '2405',
-    annualSalary: '47000',
+    name: "Atticus",
+    employeeNumber: "2405",
+    annualSalary: "47000",
     reviewRating: 3
   },
   {
-    name: 'Jem',
-    employeeNumber: '62347',
-    annualSalary: '63500',
+    name: "Jem",
+    employeeNumber: "62347",
+    annualSalary: "63500",
     reviewRating: 4
   },
   {
-    name: 'Scout',
-    employeeNumber: '6243',
-    annualSalary: '74750',
+    name: "Scout",
+    employeeNumber: "6243",
+    annualSalary: "74750",
     reviewRating: 5
   },
   {
-    name: 'Robert',
-    employeeNumber: '26835',
-    annualSalary: '66000',
+    name: "Robert",
+    employeeNumber: "26835",
+    annualSalary: "66000",
     reviewRating: 1
   },
   {
-    name: 'Mayella',
-    employeeNumber: '89068',
-    annualSalary: '35000',
+    name: "Mayella",
+    employeeNumber: "89068",
+    annualSalary: "35000",
     reviewRating: 1
   }
 ];
@@ -40,4 +40,71 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log(employees);
+
+for (let i = 0; i < employees.length; i += 1) {
+  console.log(employees[i]);
+}
+
+// function bonusCalculator(empInfo) {
+//   let newObject = {
+//     name: empInfo.name,
+//     bonusPercentage
+//   };
+
+//   return newObject;
+// }
+
+function bonusPercentage() {
+  for (let i = 0; i < employees.length; i += 1) {
+    const bonusEmployees = [];
+
+    // create new object
+    const bonusObject = {
+      name: employees[i].name,
+      bonusPercentage: "",
+      totalCompensation: "",
+      totalBonus: ""
+    };
+    // call to function to create object
+    // newEmployeeBonusObjectMachine(employees[i]);
+
+    // bonusCalculator(employees[i]);
+    let baseBonus = calculateBaseBonusPercentage(employees[i]);
+    console.log("baseBonus", baseBonus);
+
+    // baseBonus = adjustBonus(employees[i], baseBonus);
+    // push new object into new bonus array
+
+    console.log(employees);
+    bonusEmployees.push(bonusObject);
+  }
+  return;
+}
+
+function calculateBaseBonusPercentage(employee) {
+  let ratingPercentage = 0;
+  let yearsAtJobBonus = 0;
+
+  if (employee.reviewRating <= 2) {
+    ratingPercentage = 0;
+    // yearsAtJobBonus = 0.05;
+    // let percentageEarned = ratingPercentage + yearsAtJobBonus;
+    // return percentageEarned;
+    // } else if (employee.reviewRating === 3) {
+    //   ratingPercentage = 0.04;
+    // } else if (employee.reviewRating === 4) {
+    //   ratingPercentage = 0.06;
+    // } else if (employee.reviewRating === 5) {
+    //   ratingPercentage = 0.1;
+    // }
+  }
+
+  // the next check
+  if (employee.employeeNumber.length == 4) {
+    ratingPercentage = ratingPercentage + 0.05;
+  }
+  return ratingPercentage;
+}
+
+console.log(bonusPercentage());
